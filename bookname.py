3d6f7f5a7f13c64e9983525c088f972320d5bc72
SuhasBansod <suhasbansod6@gmail.com>
2024-12-07 21:48:55 +0530

    SuhasBansod: datascraping

---
+++ bookname.py
@@ -0,0 +1,17 @@
+import requests as rq
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+import pandas as pd
+
+Burl = 'https://books.toscrape.com/'
+Bheader = {'user-agent' : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36'
+
+}
+Bk=rq.get(url=Burl, headers=Bheader)
+BkBeautiful = BeautifulSoup(Bk.content,'html.parser')
+Bkfind= BkBeautiful.find_all('h3')
+nameofbook=[{'Name': i.text} for i in Bkfind]
+nameofbookdf=pd.DataFrame(nameofbook)
+nameofbookdf.to_csv('Bookname.csv')
+
+
